2a518b7cb88f660b6967c02e1a9a0f2ba4589cb1
rafaelsidney34-svg <rafaelsidney.34@gmail.com>
2026-07-29 13:36:25 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1276,7 +1276,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       const btnConnectMP = document.getElementById("btnConnectMP");
       if (btnConnectMP) {
-        btnConnectMP.href = `https://dirt-wagner-salmon-introduction.trycloudflare.com/auth/mercadopago?affiliateId=${user.id}`;
+        btnConnectMP.href = `https://d-a-w-load.onrender.com/auth/mercadopago?affiliateId=${user.id}`;
       }
     } else {
       if (notReg) notReg.style.display = "";
